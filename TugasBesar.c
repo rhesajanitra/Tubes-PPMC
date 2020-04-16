6742f5534851ebb0dcb3810cfdc4446af2a8f39d
indrayanayp <60211129+indrayanayp@users.noreply.github.com>
2020-04-16 22:29:59 +0800

    Update TugasBesar.c

--- TugasBesar.c
+++ TugasBesar.c
@@ -1,3 +1,12 @@
+/* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
+* MODUL 9 – TUGAS BESAR
+* Kelompok : 2 (Rombongan C)
+* Hari dan Tanggal : Kamis, 16 April 2020
+* Asisten (NIM) : Yunianti Khotimah (18317014)
+* Nama File : TugasBesar.c
+* Deskripsi : Mencetak kata random berdasarkan N grams
+*/ 
+
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <string.h>
